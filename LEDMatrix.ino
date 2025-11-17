18ccfd1036ff05a86f7c504a90e7dfe9bb7184c8
LameIkon <troelsjelsbak@hotmail.com>
2025-11-17 15:28:21 +0100

    sin wave works

--- LEDMatrix.ino
+++ LEDMatrix.ino
@@ -7,57 +7,9 @@ void setup() {
   matrix.begin();
 }
 
-uint8_t isFrameOn = 0;
-
-uint8_t frame[8][12] = {
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
-  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
-};
-
-void turnEntireFrame(){
-  if(isFrameOn == 1) isFrameOn = 0;
-  else isFrameOn = 1;
-
-  for(int i = 0; i < 8; i++){
-    for(int j = 0; j < 12; j++){
-      frame[i][j] = isFrameOn;
-    }
-  }
-}
-
-int x;
-int y;
-  
-void loop(){
-  matrix.renderBitmap(frame, 8, 12);
-  turnEntireFrame();
-  delay(100);
-
-  delay(20);
-
-  float time = millis() / 1000.0; // time in seconds
-
-  float sinValue = sinf(6.28 * freq * time - 1.57 + 6.28 * offset); // sinf returns -1.0 to 1.0
-  float sinRemap = remap(sinValue, -1.0, 1.0, 0.0, 6.0); // sin remapped to 0.0 to 6.0
-
-  x = floor(time);
-  y = floor(sinRemap);
-
-
-
-  Serial.println(sinRemap);
-}
-
-
 // MOLSTEDS CODE!!
-float freq = 1.0;
-float offset = 0.0;
+float freq = 0.5;
+float offset = 0.25;
 
 float lerp(float outMin, float outMax, float alpha) {
   // mixes 2 values linearly based on 0-1 range
@@ -75,4 +27,71 @@ float remap(float value, float inMin, float inMax, float outMin, float outMax) {
   alpha = constrain(alpha, 0.0, 1.0);
   return lerp(outMin, outMax, alpha);
 }
+
+uint8_t isFrameOn = 0;
+
+uint8_t frame[8][12] = {
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+  { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+};
+
+void turnEntireFrameOff(){
+  //if(isFrameOn == 1) isFrameOn = 0;
+  //else isFrameOn = 1;
+
+  for(int i = 0; i < 8; i++){
+    for(int j = 0; j < 12; j++){
+      frame[i][j] = 0;
+    }
+  }
+}
+
+void turnLEDsOn(int x, int y){
+  //turnEntireFrameOff();
+  frame[x][y] = 1;
+
+}
+
+int x;
+int y;
+  
+void loop(){
+  matrix.renderBitmap(frame, 8, 12);
+  //turnEntireFrame();
+  //delay(100);
+
+  //delay(20);
+
+  float time = millis() / 1000.0; // time in seconds
+
+
+  turnEntireFrameOff();
+
+  for(int i = 0; i < 12; i++){
+    float sinValue = sinf(6.28 * time * freq + (freq * i) - 1.57 + 6.28 * offset); // sinf returns -1.0 to 1.0
+    float sinRemap = remap(sinValue, -1.0, 1.0, 0.0, 8.0); // sin remapped to 0.0 to 8.0
+    
+
+    x = i;
+    y = (int)floor(sinRemap);
+
+    turnLEDsOn(y, x);
+  }
+
+
+  Serial.print(time);
+  Serial.print(',');
+  Serial.print(x);
+  Serial.print(',');
+  Serial.println(y);
+}
+
+
+
 
