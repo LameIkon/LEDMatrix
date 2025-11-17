2d432f243bcc22febdec13c059b9e4863bf24b8e
Jonas Molsted <jonasmolsted@gmail.com>
2025-11-17 19:26:07 +0100

    time scale fix

--- LEDMatrix.ino
+++ LEDMatrix.ino
@@ -59,31 +59,26 @@ void turnLEDsOn(int x, int y){
 float freq = 1.0;
 float offset = 0.25;
 
-float timeScale = 1.0;
+float timeScale = 0.2;
 
 int x;
 int y;
   
 void loop(){
   matrix.renderBitmap(frame, 8, 12);
-  //turnEntireFrame();
-  //delay(100);
 
-  //delay(20);
-
-  float time = millis() / 1000.0 * timeScale; // time in seconds
-
+  float time = millis() / 1000.0; // time in seconds
 
   turnEntireFrameOff();
 
   for(int i = 0; i < 12; i++){
     // float sinValue = sinf(6.28 * time * freq + (freq * i) - 1.57 + 6.28 * offset); // sinf returns -1.0 to 1.0
-    float sinValue = sinf(6.28 * (time + i / (11.0 / timeScale)) * freq - 1.57 + 6.28 * offset); // sinf returns -1.0 to 1.0
-    float sinRemap = remap(sinValue, -1.0, 1.0, 0.0, 8.0); // sin remapped to 0.0 to 8.0
+    float sinValue = sinf(6.28 * freq * i / 11 - 1.57 + 6.28 * time * timeScale); // sinf returns -1.0 to 1.0
+    float sinRemap = remap(sinValue, -1.0, 1.0, 0.0, 7.0); // sin remapped to 0.0 to 8.0
     
 
     x = i;
-    y = (int)sinRemap;
+    y = (int)round(sinRemap);
 
     turnLEDsOn(y, x);
   }
